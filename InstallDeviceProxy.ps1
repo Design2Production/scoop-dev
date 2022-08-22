a9ddec4d5feebc25dbecbe65717ba4b81d7dc092
Joseph Narai <jan@d-p.com.au>
2022-08-22 11:30:48 +1000

    DPEMS-97 Moved InstallDeviceProxy.ps1 to docs folder

--- InstallDeviceProxy.ps1
+++
@@ -1,5 +0,0 @@
-
-
-
-# Add auto update to scheduler
-Register-ScheduledTask -xml (Get-Content 'Scoop.Update.Apps.xml' | Out-String) -TaskName "Scoop.Update.Apps" -TaskPath "\DP\"
---
+++ docs/InstallDeviceProxy.ps1
@@ -0,0 +1,11 @@
+if ( $psversiontable.psversion.major -lt 6 )
+{
+    Write-Output "Powershell needs to be version 3 or greater"
+    exit
+}
+
+Write-Output "Powershell version:$($psversiontable.psversion.major)"
+
+
+# Add auto update to scheduler
+#Register-ScheduledTask -xml (Get-Content 'Scoop.Update.Apps.xml' | Out-String) -TaskName "Scoop.Update.Apps" -TaskPath "\DP\"
